a96c17229796c68a24303a9d1c7d5c90b7d67b4d
atharvgk <atharv.katyarmal@gmail.com>
2025-03-11 17:42:50 +0530

    sixth commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "I am PES2UG22CS110\n";
+  cout << "I am PES2UG22CS110 Atharv\n";
   return 0;
 }
